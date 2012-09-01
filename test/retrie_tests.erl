55f7a1f185e1d0f60bc8c5d58c8fcb477699ca81
Guillaume Bour <guillaume@bour.cc>
2012-09-02 00:22:07 +0200

    [fix] update unittest to follow commit e36c201 changes

--- test/retrie_tests.erl
+++ test/retrie_tests.erl
@@ -5,88 +5,88 @@
 -include("retrie.hrl").
 
 encode_test() ->
-	?assertEqual([#node{s=$a,min=1,max=1}]       , retrie:encode("a")),
-	?assertEqual([#node{s=$a,min=0,max=1}]       , retrie:encode("a?")),
-	?assertEqual([#node{s=$a,min=0,max=infinity}], retrie:encode("a*")),
-	?assertEqual([#node{s=$a,min=1,max=infinity}], retrie:encode("a+")),
+	?assertEqual({retrie, [#node{s=$a,min=1,max=1,do=[true]}]}       , retrie:encode("a")),
+	?assertEqual({retrie, [#node{s=$a,min=0,max=1,do=[true]}]}       , retrie:encode("a?")),
+	?assertEqual({retrie, [#node{s=$a,min=0,max=infinity,do=[true]}]}, retrie:encode("a*")),
+	?assertEqual({retrie, [#node{s=$a,min=1,max=infinity,do=[true]}]}, retrie:encode("a+")),
 
-	?assertEqual([#node{s=$a,min=1,max=1}, #node{s=$a,min=1,max=1}], retrie:encode("aa")),
-	?assertEqual([#node{s=$a,min=1,max=1}, #node{s=$b,min=1,max=1}], retrie:encode("ab")),
+	?assertEqual({retrie, [#node{s=$a,min=1,max=1}, #node{s=$a,min=1,max=1,do=[true]}]}, retrie:encode("aa")),
+	?assertEqual({retrie, [#node{s=$a,min=1,max=1}, #node{s=$b,min=1,max=1,do=[true]}]}, retrie:encode("ab")),
 	ok.
 
 reduce_test() ->
 	% reference: cannot be reduced
 	% ab -> ab
-	?assertEqual([#node{s=$a,min=1,max=1},#node{s=$b,min=1,max=1}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=1,max=1},#node{s=$b,min=1,max=1}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=1,max=1},
 		#node{s=$b,min=1,max=1}
-	])),
+	]})),
 
 	% aa -> a{2,2}
-	?assertEqual([#node{s=$a,min=2,max=2}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=2,max=2}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=1,max=1},
 		#node{s=$a,min=1,max=1}
-	])),
+	]})),
 
 	% aa? -> a{1,2}
-	?assertEqual([#node{s=$a,min=1,max=2}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=1,max=2}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=1,max=1},
 		#node{s=$a,min=0,max=1}
-	])),
+	]})),
 
 	% a?a -> a{1,2}
-	?assertEqual([#node{s=$a,min=1,max=2}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=1,max=2}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=0,max=1},
 		#node{s=$a,min=1,max=1}
-	])),
+	]})),
 
 	% aa* -> a+
-	?assertEqual([#node{s=$a,min=1,max=infinity}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=1,max=infinity}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=1,max=1},
 		#node{s=$a,min=0,max=infinity}
-	])),
+	]})),
 
 	% a*a -> a+
-	?assertEqual([#node{s=$a,min=1,max=infinity}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=1,max=infinity}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=0,max=infinity},
 		#node{s=$a,min=1,max=1}
-	])),
+	]})),
 
 	% aa+ -> a{2,infinity}
-	?assertEqual([#node{s=$a,min=2,max=infinity}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=2,max=infinity}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=1,max=1},
 		#node{s=$a,min=1,max=infinity}
-	])),
+	]})),
 
 	% a+a -> a{2,infinity}
-	?assertEqual([#node{s=$a,min=2,max=infinity}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=2,max=infinity}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=1,max=infinity},
 		#node{s=$a,min=1,max=1}
-	])),
+	]})),
 
 	% a?a? -> a{0,2}
-	?assertEqual([#node{s=$a,min=0,max=2}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=0,max=2}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=0,max=1},
 		#node{s=$a,min=0,max=1}
-	])),
+	]})),
 
 	% a?a+ -> a+
-	?assertEqual([#node{s=$a,min=1,max=infinity}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=1,max=infinity}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=0,max=1},
 		#node{s=$a,min=1,max=infinity}
-	])),
+	]})),
 
 	% a?a* -> a*
-	?assertEqual([#node{s=$a,min=0,max=infinity}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=0,max=infinity}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=0,max=1},
 		#node{s=$a,min=0,max=infinity}
-	])),
+	]})),
 
 	% a*a+ -> a+
-	?assertEqual([#node{s=$a,min=1,max=infinity}], retrie:reduce([
+	?assertEqual({retrie, [#node{s=$a,min=1,max=infinity}]}, retrie:reduce({retrie, [
 		#node{s=$a,min=0,max=infinity},
 		#node{s=$a,min=1,max=infinity}
-	])),
+	]})),
 
 	ok.
 
@@ -96,120 +96,120 @@ merge_test_() ->
 		fun(_) -> [
 		% a U a
 		{"a U a", 
-			?_assertEqual([#node{s=$a,min=1,max=1,do=[alpha,beta]}], 
+			?_assertEqual({retrie, [#node{s=$a,min=1,max=1,do=[alpha,beta]}]}, 
 				retrie:merge(
-					[#node{s=$a,min=1,max=1,do=[alpha]}], 
-					[#node{s=$a,min=1,max=1,do=[beta]}]
+					{retrie, [#node{s=$a,min=1,max=1,do=[alpha]}]}, 
+					{retrie, [#node{s=$a,min=1,max=1,do=[beta]}]}
 			))
 		},
 
-		{"a U b", ?_assertEqual([#branch{nodes=[
+		{"a U b", ?_assertEqual({retrie, [#branch{nodes=[
 				[#node{s=$a,min=1,max=1,do=[alpha]}],
 				[#node{s=$b,min=1,max=1,do=[beta]}]
-			]}], 
+			]}]}, 
 			retrie:merge(
-				[#node{s=$a,min=1,max=1,do=[alpha]}], 
-				[#node{s=$b,min=1,max=1,do=[beta]}]
+				{retrie, [#node{s=$a,min=1,max=1,do=[alpha]}]}, 
+				{retrie, [#node{s=$b,min=1,max=1,do=[beta]}]}
 			))
 		},
 
-		{"ab U ac", ?_assertEqual([
+		{"ab U ac", ?_assertEqual({retrie, [
 			#node{s=$a},
 			#branch{nodes=[
 				[#node{s=$b,do=[alpha]}],
 				[#node{s=$c,do=[beta]}]
-			]}], 
+			]}]}, 
 			retrie:merge(
-				[#node{s=$a}, #node{s=$b,do=[alpha]}], 
-				[#node{s=$a}, #node{s=$c,do=[beta]}]
+				{retrie, [#node{s=$a}, #node{s=$b,do=[alpha]}]}, 
+				{retrie, [#node{s=$a}, #node{s=$c,do=[beta]}]}
 			))
 		},
 
 		% same symbol, same min value
 	
-		{"inclusive, min1==min2. a{5,10} U a{5,15}", ?_assertEqual([
+		{"inclusive, min1==min2. a{5,10} U a{5,15}", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=10,do=[alpha,beta]},
 				#node{s=$a,min=1,max=5,do=[beta]}
-			],
+			]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=10,do=[alpha]}],
-				[#node{s=$a,min=5,max=15,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=10,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=5,max=15,do=[beta]}]}
 			))
 		},
 	
-		{"a{5,15} U a{5,10}", ?_assertEqual([
+		{"a{5,15} U a{5,10}", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=10,do=[beta,alpha]},
 				#node{s=$a,min=1,max=5,do=[alpha]}
-			],
+			]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=15,do=[alpha]}],
-				[#node{s=$a,min=5,max=10,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=15,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=5,max=10,do=[beta]}]}
 			))
 		},
 	
-		{"a{5,10}b U a{5,15}c", ?_assertEqual([
+		{"a{5,10}b U a{5,15}c", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=10},
 				#branch{nodes=[
 					[#node{s=$b,do=[alpha]}],
 					[#node{s=$c,do=[beta]}],
 					[#node{s=$a,min=1,max=5},#node{s=$c,do=[beta]}]
-			]}],
+			]}]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=10},#node{s=$b,do=[alpha]}],
-				[#node{s=$a,min=5,max=15},#node{s=$c,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=10},#node{s=$b,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=5,max=15},#node{s=$c,do=[beta]}]}
 			))
 		},
 
-		{"a{5,15}b U a{5,10}c", ?_assertEqual([
+		{"a{5,15}b U a{5,10}c", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=10},
 				#branch{nodes=[
 					[#node{s=$c,do=[beta]}],
 					[#node{s=$b,do=[alpha]}],
 					[#node{s=$a,min=1,max=5},#node{s=$b,do=[alpha]}]
-			]}],
+			]}]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=15},#node{s=$b,do=[alpha]}],
-				[#node{s=$a,min=5,max=10},#node{s=$c,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=15},#node{s=$b,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=5,max=10},#node{s=$c,do=[beta]}]}
 			))
 		},
 
 		% inclusive
 		% same symbol, but min is different and intersection is not empty
 
-		{"inclusive, different min and intersection not empty. a{5,15} U a{10,20}", ?_assertEqual([
+		{"inclusive, different min and intersection not empty. a{5,15} U a{10,20}", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=9,do=[alpha]},
 				#node{s=$a,min=1,max=6,do=[alpha,beta]},
 				#node{s=$a,min=1,max=5,do=[beta]}
-			],
+			]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=15,do=[alpha]}],
-				[#node{s=$a,min=10,max=20,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=15,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=10,max=20,do=[beta]}]}
 			))
 		},
 
-		{"a{10,20} U a{5,15}", ?_assertEqual([
+		{"a{10,20} U a{5,15}", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=9,do=[alpha]},
 				#node{s=$a,min=1,max=6,do=[alpha,beta]},
 				#node{s=$a,min=1,max=5,do=[beta]}
-			],
+			]},
 			retrie:merge(
-				[#node{s=$a,min=10,max=20,do=[beta]}],
-				[#node{s=$a,min=5,max=15,do=[alpha]}]
+				{retrie, [#node{s=$a,min=10,max=20,do=[beta]}]},
+				{retrie, [#node{s=$a,min=5,max=15,do=[alpha]}]}
 			))
 		},
 
-		{"inclusive, unlimited boundaries. a{5,15} U a{10,infinity}", ?_assertEqual([
+		{"inclusive, unlimited boundaries. a{5,15} U a{10,infinity}", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=9,do=[alpha]},
 				#node{s=$a,min=1,max=6,do=[alpha,beta]},
 				#node{s=$a,min=1,max=infinity,do=[beta]}
-			],
+			]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=15,do=[alpha]}],
-				[#node{s=$a,min=10,max=infinity,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=15,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=10,max=infinity,do=[beta]}]}
 			))
 		},
 
-		{"a{5,15}b U a{10,20}c", ?_assertEqual([
+		{"a{5,15}b U a{10,20}c", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=9},
 				#branch{nodes=[
 					[#node{s=$b,do=[alpha]}],
@@ -221,14 +221,14 @@ merge_test_() ->
 							[#node{s=$a,min=1,max=5},#node{s=$c,do=[beta]}]
 						]}
 					]
-				]}],
-				retrie:merge(
-					[#node{s=$a,min=5,max=15},#node{s=$b,do=[alpha]}],
-					[#node{s=$a,min=10,max=20},#node{s=$c,do=[beta]}]
+				]}]},
+			retrie:merge(
+					{retrie, [#node{s=$a,min=5,max=15},#node{s=$b,do=[alpha]}]},
+					{retrie, [#node{s=$a,min=10,max=20},#node{s=$c,do=[beta]}]}
 			))
 		},
 
-		{"a{10,20}c U a{5,15}b", ?_assertEqual([
+		{"a{10,20}c U a{5,15}b", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=9},
 				#branch{nodes=[
 					[#node{s=$b,do=[alpha]}],
@@ -240,44 +240,44 @@ merge_test_() ->
 							[#node{s=$a,min=1,max=5},#node{s=$c,do=[beta]}]
 						]}
 					]
-				]}],
-				retrie:merge(
-				[#node{s=$a,min=10,max=20},#node{s=$c,do=[beta]}],
-				[#node{s=$a,min=5,max=15},#node{s=$b,do=[alpha]}]
+			]}]},
+			retrie:merge(
+				{retrie, [#node{s=$a,min=10,max=20},#node{s=$c,do=[beta]}]},
+				{retrie, [#node{s=$a,min=5,max=15},#node{s=$b,do=[alpha]}]}
 			))
 		},
 
 		%% EXCLUSIVE
 	
-		{"a{5,10} U a{15,20}", ?_assertEqual([
+		{"a{5,10} U a{15,20}", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=10,do=[alpha]},
 				#node{s=$a,min=5,max=10,do=[beta]}
-			],
+			]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=10,do=[alpha]}],
-				[#node{s=$a,min=15,max=20,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=10,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=15,max=20,do=[beta]}]}
 			))
 		},
 	
-		{"a{15,20} U a{5,10}", ?_assertEqual([
+		{"a{15,20} U a{5,10}", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=10,do=[alpha]},
 				#node{s=$a,min=5,max=10,do=[beta]}
-			],
+			]},
 			retrie:merge(
-				[#node{s=$a,min=15,max=20,do=[beta]}],
-				[#node{s=$a,min=5,max=10,do=[alpha]}]
+				{retrie, [#node{s=$a,min=15,max=20,do=[beta]}]},
+				{retrie, [#node{s=$a,min=5,max=10,do=[alpha]}]}
 			))
 		},
 
-		{"a{5,10}b U a{15,20}c", ?_assertEqual([
+		{"a{5,10}b U a{15,20}c", ?_assertEqual({retrie, [
 				#node{s=$a,min=5,max=10},
 				#branch{nodes=[
 					[#node{s=$b,do=[alpha]}],
 					[#node{s=$a,min=5,max=10}, #node{s=$c,do=[beta]}]
-			]}],
+			]}]},
 			retrie:merge(
-				[#node{s=$a,min=5,max=10}, #node{s=$b,do=[alpha]}],
-				[#node{s=$a,min=15,max=20}, #node{s=$c,do=[beta]}]
+				{retrie, [#node{s=$a,min=5,max=10}, #node{s=$b,do=[alpha]}]},
+				{retrie, [#node{s=$a,min=15,max=20}, #node{s=$c,do=[beta]}]}
 			))
 		}
 
@@ -350,7 +350,7 @@ match_test_() ->
 		% not working: returns [true] at now
 		{"branching", [
 			{"a U b"       , ?_assertEqual(true , retrie:match(retrie:merge("a","b"), "a"))}
-		]};
+		]},
 
 		% does not work at now
 		{"backtracking", [
